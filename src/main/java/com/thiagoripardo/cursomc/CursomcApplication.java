9d2e292e786cb3b54d8684f45e31eb93d806b810
Thiago Ripardo <thiago.ripardo.86@gmail.com>
2019-09-08 10:55:26 -0400

    criando operacao de instaciacao

--- src/main/java/com/thiagoripardo/cursomc/CursomcApplication.java
+++ src/main/java/com/thiagoripardo/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.thiagoripardo.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.thiagoripardo.cursomc.domain.Categoria;
+import com.thiagoripardo.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository cat;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		cat.saveAll(Arrays.asList(cat1,cat2));
+		
+		
 	}
 
 }
